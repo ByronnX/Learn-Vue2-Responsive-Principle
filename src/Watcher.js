7fdc9a8e2a53ed9da2b324b4f5758a8d718653c1
xyh20001018 <1031466692@qq.com>
2023-04-24 06:48:35 +0800

    完成Dep和Watcher的清晰梳理流程

--- src/Watcher.js
+++ src/Watcher.js
@@ -1,100 +1,74 @@
+/**
+ * Watcher类做了些什么？
+ * 1、实例化的时候，调用了自己写的get方法，意义在于可以进行下面几步的操作
+ * 2、实例化watcher时，会先把watcehr实例赋值给Dep.target。
+ * 3、然后会调用getter即调用updateComponent()即vm._update(vm._render），这里会跳到defineReactive里的get(), 然后调用watcher的addDep方法； *
+ */
+/**
+ * Watcher和Dep实现订阅的步骤：
+ * 1、首先在main.js里面对某个对象的某个属性进行new Watcher实例化的注册
+ * 2、在实例化的时候，就调用了自己写的get方法
+ * 3、get()方法把自己挂载到了一个全局唯一的位置，并且结合parsePath对最深层次的值进行了get，因此调用到了defineReactive的get()方法
+ * 4、由于第三步已经把自己挂载到了一个全局唯一的位置,在defineReactive的get()方法里就调用Dep.depend()方法
+ * 5、而Dep.depend方法就是把存放在Dep.target的Watcher实例给放进subs数组里
+ */
+/**
+ * Watcher和Dep实现发布的步骤：
+ * 1、当数据发生变化的时候，触发了defineReactive的set()方法，这里调用了Dep.notify()方法
+ * 2、Dep.notify()方法里面就是把在依赖过程中订阅到的所有观察者，也就是一个个Watcher实例，都触发他们本身的的Watcher.update()方法，
+ * update()=========>run(),
+ * run()=========>getAndInvoke(),
+ * getAndInvoke()=========>cb.call(this.target, value, oldValue);
+ * 3、update()方法在源码中利用了队列做了进一步优化，在nextTick后执行所有watcher的run(),
+ * 最后执行它们的回调函数（也就是把旧值，新值都传给实例化Watcher时要怎么处理这些数据的回调函数（第三个参数）里）
+ */
 import Dep from "./Dep";
 var uid = 0;
 export default class Watcher {
-  // 观察者 也就是依赖，观察的是哪些地方用到了这个数据 哪些数据依赖着哪些地方
-  // target：监听哪个对象
-  // expression：obj.a.b.c 谁点谁点谁
   constructor(target, expression, callback) {
-    // console.log("我是Watcher的构造器");
     this.id = uid++;
     this.target = target;
-    // 执行this.getter（），就可以读取data.a.b.c的内容
     this.getter = parsePath(expression);
-    /**
-     * 这里的这个parsePath方法可以按点来拆分层级===》obj.a.b.c。
-     * 传入expreesion是可以提前根据你输入的参数，构造一个对应层级的解拆分函数给你，
-     * 比如你传入a.b.c他就知道你返回一个三层的函数给你，你传入一个三层的对象进来就刚刚好，传入两层的就会报错
-     */
     this.callback = callback;
-    this.value = this.get(); // 这里构造的时候，“刺激”调用了一下,目的是把自己设置到Dep.target上
-    // console.log("*******", this.get());
+    // 做了1
+    this.value = this.get();
   }
   update() {
-    this.run(); //6、当你数据更新的时候调用run()，现在去到该类的run方法
+    this.run();
   }
   get() {
-    // 进入依赖收集阶段.让全局的Dep.target设置为Watcher实例本身，也就是把自己这个实例对象放到全局唯一的地方，那么就是进入依赖收集的阶段
-    Dep.target = this; // this === Watcher实例本身。哪个Watcher在读get，哪个Watcher就是这个Dep.target，就会被抓到逮到拿到这个想读的对象
-    const obj = this.target; //
-    // console.log("obj==================", obj);==============>new Watcher()的第一个参数
-    // console.log("this==================", this);==============>Watcher实例本身
-    // console.log("Dep.target==================", Dep.target);==============>Watcher实例本身
-    // console.log("this.target==================", this.target);==============>new Watcher()的第一个参数
-    /**
-     * this 就是watcher实例对象本身
-     * this.target就是初始化watcher时，传给构造函数的第一个参数,也就是需要被收集依赖的对象
-     * Dep.target就是this
-     * obj就是this.target
-     */
+    // 做了2
+    Dep.target = this;
+    const obj = this.target;
     var value;
-    // 只要能找，就一直找
     try {
-      //重点代码：obj: 监视对象，调用obj的getter，就是调用defineReactive触发收集依赖
+      // 做了3
       value = this.getter(obj);
-      console.log("getter", value); //======>parsePath带上参数所返回的值int=====>6、88
-      /**
-      这里的getter就是parsePath返回的fn对象，然后把这个obj往里面调用
-      ƒ (obj) {
-          for (var i = 0; i < segments.length; i++) {
-            if (!obj) return;
-            obj = obj[segments[i]]; 
-          }
-          return obj;
-      }
-      这里的value也就是this.getter(obj) 就是value================== 6获取到最终需要的值，也就是a.m.n的值
-      */
-      // console.log("this.getter==================", this.getter);======>parsePath的返回值
-      // console.log("value==================", value);
     } finally {
       Dep.target = null;
     }
     return value;
   }
   run() {
-    this.getAndInvoke(this.callback); //7、这个run方法把new Watcher的第三个参数传了进来调用了一个getAndInvoke方法，现在去到本类的getAndInvoke方法
-    // console.log("this.callback", this.callback);=========》就是new Watcher的第三个参数
+    this.getAndInvoke(this.callback);
   }
   getAndInvoke(cb) {
-    // 发布依赖的地方并且把新旧值给传递call对象也就是开始设置，这里是妙笔
     const value = this.get();
-    // console.log("value", value); //==========》88
-
     if (value !== this.value || typeof value == "object") {
-      // 如果这里的值改变了，或者是对象形式
       const oldValue = this.value;
-      // console.log("oldValue", oldValue); =========》6
-
       this.value = value;
-      // console.log("value", value);===========》8
-
-      cb.call(this.target, value, oldValue); // 8、设置并完成更新
+      cb.call(this.target, value, oldValue);
     }
+    console.log("cb====", cb);
   }
 }
-
 function parsePath(str) {
   var segments = str.split(".");
-
   return obj => {
     for (let i = 0; i < segments.length; i++) {
       if (!obj) return;
-      obj = obj[segments[i]]; // 就是说abcd都是对象的属性，然后一层一层去取，obj[a][b][c][d]
+      obj = obj[segments[i]];
     }
     return obj;
   };
 }
-
-/**
- * 什么是Watcher？
- * Watcher是一个中介角色，数据发生变化时通知它，然后它在通知其他地方。
- */
--- src/main.js
+++ src/main.js
@@ -12,28 +12,6 @@ var obj = {
 
 observe(obj);
 new Watcher(obj, "a.m.n", val => {
-  // 这里实例化watcher的时候，也把自己设置到了Dep.target上
   console.log("牛逼！！我监控到了a.m.n的值是", val);
 });
-obj.a.m.n = 88; // 1、这里触发了defineReactive的get()
-// console.log(obj);
-
-/*
-    作用详解:
-    getter 方法完成的工作就是依赖收集 —— dep.depend()
-    setter 方法完成的工作就是发布更新 —— dep.notify()
-    Watcher（订阅者）：有get和update方法
-    调度中心作用的 Dep：
-    1.收集订阅者 Watcher 并添加到观察者列表 subs
-    2.接收发布者的事件
-    3.通知订阅者目标更新，让订阅者执行自己的 update 方法
-    vue组件1->watcher1->dep
-    vue组件2->watcher2->dep
-    watcher是连接vue组件和dep的桥梁
-    每个组件对应一个watcher，每个属性对应一个dep
-    data1->name:‘wyh’->dep
-    data2->name:‘aaa’->dep
-    dep中getter和setter有两个方法
-    getter->dep.depend()收集依赖
-    setter_>dep.notify()通知更新
-*/
+obj.a.m.n = 88;
